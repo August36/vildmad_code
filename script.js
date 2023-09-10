012a85517bde31076e9b0896c3ed1ed38fb18e73
August <augustjp@live.dk>
2023-09-10 15:09:30 +0200

    produkt img is not dynamic - skovsyre

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // https://mlbkadeqohvoalvuqhec.supabase.co
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sYmthZGVxb2h2b2FsdnVxaGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1MDgzNjYsImV4cCI6MjAwOTA4NDM2Nn0._KaG9H7dvxEtQBOqnpjnmdLml5vjSigGXSXpGtmHur8
 // select=id,title,season/0,season/1,season/2,season/3,categories/0/name,sankelandskaber/0/title,sankelandskaber/0/profile_image/src,sankelandskaber/0/profile_image/id
-fetch("https://mlbkadeqohvoalvuqhec.supabase.co/rest/v1/vildmad_gruppe11?select=id,title", {
+fetch("https://mlbkadeqohvoalvuqhec.supabase.co/rest/v1/vildmad_gruppe11", {
     method: "GET",
     headers: {
         apikey:
@@ -13,6 +13,7 @@ fetch("https://mlbkadeqohvoalvuqhec.supabase.co/rest/v1/vildmad_gruppe11?select=
 
 function showData(items){
     console.log(items);
+    document.querySelector(".product_img img").src="https://vildmadv2.vps.webdock.io/application/files/8416/2401/2670/Alm-syre_ravarkort_app.png"
     // items.forEach()
 }
 
