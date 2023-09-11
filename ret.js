37639c330f45ac66dd915a3a3a3bdccfe10578f6
August <augustjp@live.dk>
2023-09-11 21:32:59 +0200

    found out how to target different rows and columns

--- ret.js
+++ ret.js
@@ -1,7 +1,7 @@
 //Jeg har fundet ud af hvordan jeg gør html elementerne dynamiske, men kun med rows som hører under columnen 'ret'.
 //  Det har jeg gjort ved at tilføje ?select=ret i URL'en. Men hvordan tilføjer jeg de andre columns??
 
-fetch(`https://mlbkadeqohvoalvuqhec.supabase.co/rest/v1/vildmad_gruppe11?select=ret`, {
+fetch(`https://mlbkadeqohvoalvuqhec.supabase.co/rest/v1/vildmad_gruppe11`, {
   method: "GET",
   headers: {
     apikey:
@@ -10,15 +10,17 @@ fetch(`https://mlbkadeqohvoalvuqhec.supabase.co/rest/v1/vildmad_gruppe11?select=
 })
 .then(response => response.json())
 .then((data) => {
-  showH1(data[0]); // Targets h1
+  showH1(data[0]); // Targets h1 - the [number] selects which row i chose
   showH2(data[3]); // Targets h2
-  showP1(data[4]); // targets den første p1 (.ingredient_amount_paragraph) på siden. Skift data ud med det rigtige fra databasen.
+  showP1(data[4]); // targets first paragraph on site
+  showP2(data[2]);
+  showP3(data[2]);
 });
 
 function showH1(product){
     if (product) {
         console.log(product);
-        document.querySelector("h1").textContent = product.ret;
+        document.querySelector("h1").textContent = product.ret; // the text after product. selects the column. in this case the column 'ret'. write name of any column in supabase table to change it
     } else {
         console.log("no data found");
     }
@@ -34,7 +36,23 @@ function showH2(product) {
 
 function showP1(product) {
     if (product) {
-        document.querySelector(".ingredient_amount_paragraph").textContent = product.ret;
+        document.querySelector(".ingredient_amount_paragraph").textContent = product.categories;
+    } else {
+        console.log("No data found for the paragraph");
+    }
+}
+
+function showP2(product) {
+    if (product) {
+        document.querySelector(".recipe1").textContent = product.opskrift1;
+    } else {
+        console.log("No data found for the paragraph");
+    }
+}
+
+function showP3(product) {
+    if (product) {
+        document.querySelector(".recipe2").textContent = product.opskrift2;
     } else {
         console.log("No data found for the paragraph");
     }
